96037354bce7a8c25823e6337e2178e532794ae9
Julian-4A5A <504690@edu.rocmn.nl>
2017-03-20 08:57:02 +0100

    a

--- Project.php
+++ Project.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="Style.css">
     <script src="Javascript.js" charset="utf-8"></script>
   </head>
-  <body>
+  <body id="basis">
 
       <section class="row">
         <?php include 'Header.php'; ?>
@@ -22,14 +22,13 @@
               <img src="http://placehold.it/300x200">
           </div>
           <div>
-
             <p class="Text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni
              m ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
              iquip ex ea commodo consequat. Duis aute irure dolor in reprehenderi
             t in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except
             eur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p><button id="button" type="button" name="button">View More</button></p>
+            <p><button id="button" type="button" onclick="document.GetEllementById('basis')= loadpage(projecten/schoolopdrachten.php, callback;)">View More</button></p>
           </div>
         </div>
         <div class="col-4">
@@ -39,7 +38,7 @@
           <div class="Image">
             <img src="http://placehold.it/300x200">
           </div>
-          <div >
+          <div>
             <p class="Text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni
              m ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
@@ -56,14 +55,15 @@
           <div class="Image">
             <img src="http://placehold.it/300x200">
           </div>
-          <div >
-            <p class="Text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni
-             m ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
-             iquip ex ea commodo consequat. Duis aute irure dolor in reprehenderi
-            t in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except
-            eur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+          <div>
+            <p class="Text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni
+              m ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut al
+              iquip ex ea commodo consequat. Duis aute irure dolor in reprehenderi
+              t in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except
+              eur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
             <p><button id="button" type="button" name="button">View More</button></p>
           </div>
         </div>
--- database/conn.php
+++ database/conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Portfolio";
+$dbname = "portfolio";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
